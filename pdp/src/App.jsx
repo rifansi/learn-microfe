b39902cdc909a1dcea0d9ddc4c27e554fe3afd04
Rifan M Fauzi <rfauzi@softint.com.au>
2023-03-11 13:46:31 +0700

    export header & footer from home project and import & use it in pdp project

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.scss";
 
+import Footer from "home/Footer";
+import Header from "home/Header";
+
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: pdp</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="flex flex-col min-h-screen max-w-7xl mx-auto">
+    <Header></Header>
+    <main className="container mx-auto py-8 px-4 flex-1">PDP content</main>
+    <Footer className="self-end"></Footer>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
