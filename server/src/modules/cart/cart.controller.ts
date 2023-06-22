088bef75f8f89e1ee69d469523629ffc955a7b03
Rifan M Fauzi <rfauzi@softint.com.au>
2023-06-22 21:29:31 +0700

    add endpoint for create & delete item from cart

--- server/src/modules/cart/cart.controller.ts
+++ server/src/modules/cart/cart.controller.ts
@@ -1,13 +1,63 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
+import { Product, products } from '../../product';
+
+interface CartItem extends Product {
+  quantity: number;
+}
+
+interface Cart {
+  items: CartItem[];
+}
+
+const initialCart = (indexes: number[]): Cart => ({
+  items: indexes.map((index) => ({
+    ...products[index],
+    quantity: 1,
+  })),
+});
 
 @Controller('cart')
 export class CartController {
+  private carts: Record<number, Cart> = {
+    1: initialCart([0, 1]),
+    2: initialCart([1, 2]),
+  };
+
   constructor() {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
-  async index(@Request() req): Promise<{ userId: number }> {
-    return { userId: req.user.userId };
+  async index(@Request() req): Promise<Cart> {
+    return this.carts[req.user.userId] ?? { items: [] };
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(@Request() req, @Body() { id }: { id: string }): Promise<Cart> {
+    const cart = this.carts[req.user.userId];
+    const cartItem = cart.items.find((item) => item.id === parseInt(id));
+    if (cartItem) {
+      cartItem.quantity++;
+    } else {
+      cart.items.push({ ...products[parseInt(id)], quantity: 1 });
+    }
+    return cart;
+  }
+
+  @Delete()
+  @UseGuards(JwtAuthGuard)
+  async delete(@Request() req): Promise<Cart> {
+    const cart = this.carts[req.user.userId];
+    cart.items = [];
+    return cart;
   }
 }
